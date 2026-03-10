092b496c31ea66a93df8fd41268b49734a69ca5a
ErichNguyen1312 <vinhnguyenfit1312@gmail.com>
2026-03-10 16:55:30 +0700

    migrate vue to react

---
+++ backend/src/main/java/tdc/nhom12/bookstore/controller/DemoController.java
@@ -0,0 +1,18 @@
+package tdc.nhom12.bookstore.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+    @GetMapping("/demo")
+    public String demo() {
+        return "Hello World!";
+    }
+
+    @GetMapping("/test-docker")
+    public String testOnDocker() {
+        return "Hello World on docker container!";
+    }
+
+}
